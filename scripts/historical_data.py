f77dec74729335e5110b037cf4deff058eae3310
Bahadır Bör <70104032+bahadirbor@users.noreply.github.com>
2025-05-30 13:57:03 +0300

    Created hourly_to_daily function
    hourly_to_daily function created to extract and fetch data

--- scripts/historical_data.py
+++ scripts/historical_data.py
@@ -2,7 +2,16 @@ from meteostat import Hourly
 from datetime import datetime
 import pandas as pd
 
-#Added cities for weather observation datas
+
+def hourly_to_daily(nearest_station_id, starting_time, ending_time):
+    # Data extraction and fetch
+    data = Hourly(nearest_station_id, starting_time, ending_time)
+    data_frame = data.fetch()
+    data_frame.reset_index(inplace=True)
+    data_frame["time"] = pd.to_datetime(data_frame["time"])
+
+
+# Added cities for weather observation datas
 cities = {
     "İstanbul": "17060",
     "Ankara": "LTAB0",
@@ -11,6 +20,6 @@ cities = {
     "Adana": "17352"
 }
 
-start_time = datetime(2020,1,1)
-end_time = datetime(2024,12,31)
+start_time = datetime(2020, 1, 1)
+end_time = datetime(2024, 12, 31)
 
